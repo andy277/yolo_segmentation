d585f4bcd02d9a58db72b3edb5f7fbd73a39de96
Andy Nguyen <andyminh05@gmail.com>
2024-09-26 12:35:43 -0700

    test more attributes

--- test/test_attributes.py
+++ test/test_attributes.py
@@ -3,10 +3,11 @@ from core.detect import Segmentation
 import logging
 
 logging.basicConfig(filename="logs/log.txt",
-                    level=logging.INFO, filemode="w",
+                    level=logging.INFO, filemode="a",
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%H:%M:%S',)
 logger = logging.getLogger(__name__)
+
 
 class MyTestCase(unittest.TestCase):
     def test_attributes(self):
@@ -31,6 +32,36 @@ class MyTestCase(unittest.TestCase):
         #masks
         masks = results.masks
         self.assertIsNotNone(masks)
+
+        #probs
+        # probs = results.probs
+        # self.assertIsNotNone(probs)
+        #Probs is None
+
+        #keypoints
+        # keypoints = results.keypoints
+        # self.assertIsNotNone(keypoints)
+        #Keypoints is None
+
+        #obb
+        # obb = results.obb
+        # self.assertIsNotNone(obb)
+        #obb is None
+
+        #speed
+        speed = results.speed
+        self.assertIsNotNone(speed)
+
+        #names
+        names = results.names
+        self.assertIsNotNone(names)
+
+        #path
+        path = results.path
+        self.assertIsNotNone(path)
+
+        results.show()
+        logger.info('Tests run')
 
 
 if __name__ == '__main__':
